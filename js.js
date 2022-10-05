4e4ce1cb914207181a3275abd4106ca03236c845
Anastasiia_Korin <akoren65787@gmail.com>
2022-10-05 21:14:16 +0200

    Created Alex task

---
+++ js.js
@@ -0,0 +1,9 @@
+// find element
+let banner = document.getElementById('banner');
+
+// change color
+banner.style.background = 'pink';
+
+// change text
+document.getElementById('banner-text').innerHTML='Team Root';
+
