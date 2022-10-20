ec4d8db603684c415eb4e3b842c2bff43eba65b4
Anastasiia_Korin <akoren65787@gmail.com>
2022-10-20 12:37:53 +0200

    Change text and color

--- js.js
+++ js.js
@@ -1,9 +1,75 @@
 // find element
-let banner = document.getElementById('banner');
+/*
+const banner = document.getElementById('banner');
 
 // change color
 banner.style.background = 'pink';
 
 // change text
-document.getElementById('banner-text').innerHTML='Team Root';
+document.getElementById('banner-text').innerHTML='Team Root';*/
 
+/*
+document.getElementById('banner-text').onclick = changeText;   
+
+    function changeText() {
+    if
+        (banner-text.text = 'Root Team'){
+            banner-text.text = 'Root Team 2 ';  
+        }
+    else 
+        (banner-text.text = 'Root Team 2 '){
+            banner-text.text = 'Root Team';
+        }
+    
+    }  */ 
+    
+    
+    
+    banner.addEventListener('click',()=> {
+        console.log('click')
+        if (banner.style.backgroundColor === 'rgb(156, 50, 172)'){
+            banner.style.backgroundColor = 'rgb(244, 250, 89)'
+        }
+
+        else if (banner.style.backgroundColor === 'rgb(244, 250, 89)'){
+            banner.style.backgroundColor = 'rgb(156, 50, 172)'
+        } 
+    })
+
+/*
+      document.getElementById('banner-text').onclick = function() {
+    this.textContent = 'Team Root 2';
+
+     
+} */
+
+
+/*  Через масив
+const banner = document.getElementById('ban');
+
+const colors = ["red", "blue", "yellow"];
+
+banner.addEventListener("click", () => {
+    document.style.backgroundColor = colors[getRandomNumber()];
+});
+
+function getRandomNumber () {
+    return Math.floor(Math.random() * colors.length);
+}*/
+
+
+
+/*
+$(document).ready(function () {
+    $("#banner").click(function () {
+      $("#banner-text").text("Team Root 2");
+    } )
+  } );   */  
+
+  $(document).ready(function () {
+    $("#banner").click(function () {
+        $("#banner-text").fadeOut(function () {
+            $("#banner-text").text(($("#banner-text").text() == 'Team Root 2 ') ? 'Team Root 3' : 'Team Root 2 ').fadeIn();
+        })
+    })
+});
